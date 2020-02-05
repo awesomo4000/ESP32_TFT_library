60cc1d844f574e506079519f5bd0b6596c6cab3a
aarhodes <aarhodes@cisco.com>
2020-02-05 16:09:42 -0500

    Add options for OS2020 ST7735 Greentab3 128x128

--- components/tft/tftspi.h
+++ components/tft/tftspi.h
@@ -153,6 +153,36 @@
 #define PIN_BCKL_OFF  1     // GPIO value for backlight OFF
 // --------------------------------------------------------
 
+#elif CONFIG_TFT_PREDEFINED_DISPLAY_TYPE == 5
+
+// ** Set the correct configuration for OS2020 ST7735R GREENTAB3 128x128
+// ---------------------------------------------------------------------
+#define DEFAULT_DISP_TYPE           DISP_TYPE_ST7735R
+#define DEFAULT_TFT_DISPLAY_WIDTH   128
+#define DEFAULT_TFT_DISPLAY_HEIGHT  128
+#define DISP_COLOR_BITS_24          0x66
+#define DEFAULT_GAMMA_CURVE         0
+#define DEFAULT_SPI_CLOCK           26000000
+#define TFT_INVERT_ROTATION         0
+#define TFT_INVERT_ROTATION1        1
+#define TFT_INVERT_ROTATION2        0
+#define TFT_RGB_BGR                 0x08
+
+#define USE_TOUCH   TOUCH_TYPE_NONE
+
+#define PIN_NUM_MISO 19     // SPI MISO
+#define PIN_NUM_MOSI 23     // SPI MOSI
+#define PIN_NUM_CLK  18     // SPI CLOCK pin
+#define PIN_NUM_CS   19     // Display CS pin
+#define PIN_NUM_DC   26    // Display command/data pin
+#define PIN_NUM_TCS   0     // Touch screen CS pin
+
+#define PIN_NUM_RST  25     // GPIO used for RESET control
+#define PIN_NUM_BCKL  5     // GPIO used for backlight control
+#define PIN_BCKL_ON   0     // GPIO value for backlight ON
+#define PIN_BCKL_OFF  1     // GPIO value for backlight OFF
+// --------------------------------------------------------
+
 #else
 
 // Configuration for other boards, set the correct values for the display used
